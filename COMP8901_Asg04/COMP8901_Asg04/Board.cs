85c9deb5135c6b5af2f0496e303399346fe06c68
Erick FdA <erick.fernandez.da@gmail.com>
2017-10-31 17:08:50 -0700

    Initial Commit

---
+++ COMP8901_Asg04/COMP8901_Asg04/Board.cs
@@ -0,0 +1,148 @@
+﻿/*========================================================================================
+    Board                                                                            *//**
+	
+    A Connect 4 game board.
+	
+    Copyright 2017 Erick Fernandez de Arteaga. All rights reserved.
+        https://www.linkedin.com/in/erick-fda
+        https://bitbucket.org/erick-fda
+        
+	@author Erick Fernandez de Arteaga
+    @version 0.0.0
+	@file
+	
+*//*====================================================================================*/
+
+/*========================================================================================
+	Dependencies
+========================================================================================*/
+using SysGeneric = System.Collections.Generic;
+
+/*========================================================================================
+	Board
+========================================================================================*/
+/**
+    A Connect 4 game board.
+*/
+public class Board
+{
+    /*------------------------------------------------------------------------------------
+		Instance Fields
+	------------------------------------------------------------------------------------*/
+    public const int BOARD_WIDTH = 7;
+    public const int BOARD_HEIGHT = 6;
+
+    private const char EMPTY = ' ';
+    public const char PLAYER_1_PIECE = 'X';
+    public const char PLAYER_2_PIECE = 'O';
+
+    //private const char ROW_SEPARATOR_CHAR = '-';
+    //private readonly string ROW_SEPARATOR;
+    //private const string CELL_SEPARATOR = "|";
+
+    private const string CELL_SEPARATOR_BEFORE = "[";
+    private const string CELL_SEPARATOR_AFTER = "]";
+
+    private const int MAX_MOVES_IN_GAME = BOARD_WIDTH * BOARD_HEIGHT;
+
+    /*------------------------------------------------------------------------------------
+		Instance Properties
+	------------------------------------------------------------------------------------*/
+    SysGeneric.List<SysGeneric.List<char>> _board { get; set; }
+    int _moveCount { get; set; }
+    string _moves { get; set; }
+    SysGeneric.List<int> _piecesInColumn { get; set; }
+
+    /*------------------------------------------------------------------------------------
+		Constructors & Destructors
+	------------------------------------------------------------------------------------*/
+    public Board()
+    {
+        /* Initialize the board to all spaces empty. */
+        _board = new SysGeneric.List<SysGeneric.List<char>>();
+        for (int columnNum = 0; columnNum < BOARD_WIDTH; columnNum++)
+        {
+            SysGeneric.List<char> column = new SysGeneric.List<char>();
+
+            for (int cellNum = 0; cellNum < BOARD_HEIGHT; cellNum++)
+            {
+                column.Add(EMPTY);
+            }
+
+            _board.Add(column);
+        }
+
+        ///* Initialize the row separator string. */
+        //ROW_SEPARATOR = new System.String(ROW_SEPARATOR_CHAR, BOARD_WIDTH * 2 + 1);
+
+        _moveCount = 0;
+        _moves = "";
+        _piecesInColumn = new SysGeneric.List<int>(BOARD_WIDTH);
+    }
+
+    /*------------------------------------------------------------------------------------
+		Instance Methods
+	------------------------------------------------------------------------------------*/
+    /*
+        Tries to make a particular move on the game board.
+        Returns whether or not the move was made.
+    */
+    //public bool TryMove(char piece, int column)
+    //{
+    //    if (IsMoveValid(column))
+    //    {
+    //        _board[]
+
+    //        return true;
+    //    }
+
+    //    return false;
+    //}
+
+    public bool IsMoveValid(int column)
+    {
+        /* Fail if the column number is invalid. */
+        if (column < 0 ||
+            column > BOARD_WIDTH - 1)
+        {
+            System.Console.Write("That's not a valid column number! Please enter a valid column number.\n");
+            return false;
+        }
+
+        /* Fail if the column is full. */
+        if (_piecesInColumn[column] >= BOARD_HEIGHT)
+        {
+            System.Console.Write("That column is full! Please choose another column.\n");
+            return false;
+        }
+
+        return true;
+    }
+
+    public override string ToString()
+    {
+        string stringVal = "";
+
+        /* Print cells. */
+        for (int rowNum = 0; rowNum < BOARD_HEIGHT; rowNum++)
+        {
+            for (int columnNum = 0; columnNum < BOARD_WIDTH; columnNum++)
+            {
+                stringVal += CELL_SEPARATOR_BEFORE + _board[columnNum][rowNum] + CELL_SEPARATOR_AFTER;
+            }
+
+            stringVal += "\n";
+        }
+
+        /* Print column numbers. */
+        stringVal += " ";
+        for (int i = 0; i < BOARD_WIDTH; i++)
+        {
+            stringVal += (i + 1) + "  ";
+        }
+
+        stringVal += "\n\n";
+
+        return stringVal;
+    }
+}
---
+++ COMP8901_Asg04/COMP8901_Asg04/Program.cs
@@ -0,0 +1,170 @@
+﻿/*========================================================================================
+    COMP8901 Assignment 04                                                           *//**
+	
+    A Connect 4 game with an AI opponent that uses the negamax algorithm with alpha and 
+    beta pruning to choose moves.
+	
+    Copyright 2017 Erick Fernandez de Arteaga. All rights reserved.
+        https://www.linkedin.com/in/erick-fda
+        https://bitbucket.org/erick-fda
+        
+	@author Erick Fernandez de Arteaga
+    @version 0.0.0
+	@file
+	
+*//*====================================================================================*/
+
+/*========================================================================================
+	Dependencies
+========================================================================================*/
+
+
+/*========================================================================================
+	MyProgram
+========================================================================================*/
+/**
+    A Connect 4 game with an AI opponent that uses the negamax algorithm with alpha and 
+    beta pruning to choose moves.
+*/
+namespace COMP8901_Asg04
+{
+    class Program
+    {
+        /*--------------------------------------------------------------------------------
+            Class Fields
+        --------------------------------------------------------------------------------*/
+
+
+        /*--------------------------------------------------------------------------------
+            Class Properties
+        --------------------------------------------------------------------------------*/
+        static private Board _gameBoard { get; set; }
+        static private bool _playerGoesFirst { get; set; }
+        static private bool _isFirstTurn { get; set; }
+        static private char _playerPiece { get; set; }
+        static private char _opponentPiece { get; set; }
+        static private bool _roundOver { get; set; }
+        static private bool _gameOver { get; set; }
+
+        /*--------------------------------------------------------------------------------
+            Main Method
+        --------------------------------------------------------------------------------*/
+        /**
+            Plays a game of Connect 4.
+        */
+        static void Main(string[] args)
+        {
+            Init();
+
+            while (!_gameOver)
+            {
+                PlayRound();
+            }
+
+            System.Console.Write("Thanks for playing! Press ENTER to exit...\n\n");
+            System.Console.ReadLine();
+            return;
+        }
+
+        /*--------------------------------------------------------------------------------
+            Class Methods
+        --------------------------------------------------------------------------------*/
+        /**
+            Initializes fields.
+        */
+        static private void Init()
+        {
+            _gameBoard = new Board();
+            _playerGoesFirst = true;
+            _isFirstTurn = true;
+            _roundOver = false;
+            _gameOver = false;
+        }
+
+        /**
+            Play a round of Connect 4.
+        */
+        static void PlayRound()
+        {
+            DecideTurnOrder();
+
+            while (!_gameOver)
+            {
+                /* Player's Turn */
+                if (!_isFirstTurn || _playerGoesFirst)
+                {
+                    PlayerTurn();
+                }
+
+                /* Check if game over. */
+
+                /* AI's Turn */
+
+                /* Check if game over. */
+
+            }
+        }
+
+        /**
+            Decide who will go first.
+        */
+        static void DecideTurnOrder()
+        {
+            while (true)
+            {
+                System.Console.Write(System.String.Format("Would you like to go first (play as {0}) this round? (Y/N)\n>> ", Board.PLAYER_1_PIECE));
+                string response = System.Console.ReadLine();
+                System.Console.Write("\n\n");
+
+                if (response.Length > 0)
+                {
+                    if (char.ToUpper(response[0]) == 'Y')
+                    {
+                        _playerGoesFirst = true;
+                        _playerPiece = Board.PLAYER_1_PIECE;
+                        _opponentPiece = Board.PLAYER_2_PIECE;
+                    }
+                    else
+                    {
+                        _playerGoesFirst = false;
+                        _playerPiece = Board.PLAYER_2_PIECE;
+                        _opponentPiece = Board.PLAYER_1_PIECE;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+
+        /**
+            Gets a move from the player.
+        */
+        static private void PlayerTurn()
+        {
+            System.Console.Write("<< Player's Turn >>\n\n");
+            System.Console.Write(_gameBoard);
+
+            /* Keep asking for a column number until a valid answer is given. */
+            while (true)
+            {
+                System.Console.Write(System.String.Format("Enter a column number to place your next piece. (1-{0})\n>> ", Board.BOARD_WIDTH));
+                string response = System.Console.ReadLine();
+                System.Console.Write("\n\n");
+                
+                if (response.Length > 0)
+                {
+                    int columnToTry = ((int)char.GetNumericValue(response[0])) - 1;
+
+                    //if (_gameBoard.TryMove(_playerPiece, columnToTry))
+                    //{
+                    //    break;
+                    //}
+                }
+            }
+
+
+        }
+    }
+}
+
